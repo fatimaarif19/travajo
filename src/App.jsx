ab8f70ef38cbd53323a7f3c6357aa74660fa8cce
fatimaarif19 <fatima.arif13071989@gmail.com>
2025-07-19 04:21:44 +0530

    added worker's dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import Landing from "./pages/Landing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
+import WorkerDB from "./pages/WorkerDB.jsx";
 
 function App() {
-  return <Landing />;
+  return (
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/workerdb" element={<WorkerDB />} />
+      </Routes>
+  );
 }
 
 export default App;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import bg from "../assets/bg.jpg";
 
 export default function Landing() {
@@ -11,14 +11,18 @@ export default function Landing() {
     >
       <h1 className="text-9xl font-bold">TRABAJO</h1>
       <p className="text-4xl mt-2 italic">- bridging the gap -</p>
-      <div className="mt-10 flex gap-6"></div> 
-      <button className="bg-green-600 text-white px-4 py-2 rounded-xl hover:bg-green-700 transition duration-300 font-medium">
-      Looking for Work?
-      </button>
-      <div className="mt-3 flex gap-2"></div>     
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700 transition duration-300 font-medium">
-      Looking for Workers?
-      </button>
+      <div className="mt-10 flex gap-6">
+        <Link to="/workerdb">
+          <button className="bg-green-600 text-white px-4 py-2 rounded-xl hover:bg-green-700 transition duration-300 font-medium">
+            Looking for Work?
+          </button>
+        </Link>
+      </div>
+      <div className="mt-3 flex gap-2">
+        <button className="bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700 transition duration-300 font-medium">
+          Looking for Workers?
+        </button>
+      </div>
     </div>
   );
 }
